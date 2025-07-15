ac5234f5f78e08e74d2e0ce6d4320bfdef9e7687
corbin chi <corbin50111@gmail.com>
2025-07-16 01:40:39 +0800

    navbar 架大致

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
+import NavBar from './components/NavBar.vue';
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <NavBar />
+  <!-- <h1 class="text-3xl font-bold underline">Hello world!</h1> -->
 </template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<script setup>
+import LOGO from '@/assets/img/LOGO.jpg';
+</script>
+
+<template>
+  <nav class="h-[100px] bg-white shadow-md flex flex-row">
+    <div class="border border-red-500 flex-2/12 flex justify-center items-center">
+      <img :src="LOGO" class="w-[60px] h-[60px]" />
+    </div>
+    <ul class="flex flex-row gap-[16px] flex-8/12 justify-center items-center border border-amber-300">
+      <li>全部書籍</li>
+      <li>精選書單</li>
+      <li>作家專區</li>
+      <li>閱讀專欄</li>
+      <li>最新動態</li>
+      <li>關於大田</li>
+      <li>讀者專區</li>
+    </ul>
+    <div class="flex-2/12 border border-amber-600"></div>
+  </nav>
+</template>
