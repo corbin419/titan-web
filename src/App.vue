f747ae4e56119462661a9d7cb7b34f9031eb7359
紀元翔 <corbin50111@gmail.com>
2025-07-21 11:03:36 +0800

    swiper card first verson

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
 import NavBar from './components/NavBar.vue';
-
+import SwiperCard from './components/SwiperCard.vue';
 </script>
 
 <template>
   <NavBar />
+  <SwiperCard />
   <!-- <h1 class="text-3xl font-bold underline ">Hello world!</h1> -->
 </template>
