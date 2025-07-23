1ff17818c533e16ac43d868cfb6e89fb425052fb
corbin chi <corbin50111@gmail.com>
2025-07-24 00:27:00 +0800

    homepage 第一版 缺 推薦區塊

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup>
 import NavBar from './components/NavBar.vue';
 import HomePage from './views/HomePage.vue';
-// import SwiperCard from './components/SwiperCard.vue';
 </script>
 
 <template>
   <NavBar />
+  <HomePage />
   <HomePage />
 </template>
